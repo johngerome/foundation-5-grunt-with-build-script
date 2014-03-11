d1770c13ee24143e7ff9bfa11e1114c896d21eef
Gerome <gerome_dr@desiredranking.com>
2014-03-11 05:02:46 +0100

    updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,19 +16,36 @@ module.exports = function(grunt) {
       }
     },
 
+  // JS COMPILATION
+   uglify: {
+      build: {
+        options: {
+          mangle: false,
+          beautify: true,
+        },
+        files: {
+          'js/scripts.js': ['bower_components/foundation/js/vendor/jquery.js', 'bower_components/foundation/js/foundation.js'],
+          'js/modernizr.js': ['bower_components/foundation/js/vendor/modernizr.js'],
+          'js/app.min.js': ['js/app.js']
+        }
+      }
+   },
+
     watch: {
+      options: { nospawn: true },
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
         files: 'scss/**/*.scss',
         tasks: ['sass']
-      }
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass','uglify']);
   grunt.registerTask('default', ['build','watch']);
 }
