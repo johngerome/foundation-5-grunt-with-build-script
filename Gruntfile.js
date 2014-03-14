53c6bc3aa53b780032eee4de35063f86ec9e9773
Gerome <gerome_dr@desiredranking.com>
2014-03-14 10:50:44 +0100

    updated grunt script

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,30 +3,52 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
+      watch: {
         options: {
+          includePaths: ['bower_components/foundation/scss']
+        },
+        dist: {
+          options: {
+            outputStyle: 'compressed'
+          },
+          files: {
+            'css/app.css': 'scss/app.scss'
+          }        
+        }
+      },
+
+      build: {
+        options: {
+          includePaths: ['bower_components/foundation/scss'],
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
-        }        
+          'build/css/app.min..css': 'scss/app.scss'
+        }
       }
+
     },
 
-  // JS COMPILATION
    uglify: {
+      watch: {
+        options: {
+          mangle: false,
+        },
+        files: {
+          'js/scripts.min.js': ['bower_components/foundation/js/vendor/jquery.js', 'bower_components/foundation/js/foundation.js'],
+          'js/modernizr.min.js': ['bower_components/foundation/js/vendor/modernizr.js'],
+          'js/app.min.js': ['js/app.js']
+        }
+      },
+
       build: {
         options: {
           mangle: false,
-          beautify: true,
         },
         files: {
-          'js/scripts.js': ['bower_components/foundation/js/vendor/jquery.js', 'bower_components/foundation/js/foundation.js'],
-          'js/modernizr.js': ['bower_components/foundation/js/vendor/modernizr.js'],
-          'js/app.min.js': ['js/app.js']
+          'build/js/scripts.min.js': ['bower_components/foundation/js/vendor/jquery.js', 'bower_components/foundation/js/foundation.js'],
+          'build/js/modernizr.min.js': ['bower_components/foundation/js/vendor/modernizr.js'],
+          'build/js/app.min.min.js': ['js/app.js']
         }
       }
    },
@@ -37,8 +59,12 @@ module.exports = function(grunt) {
 
       sass: {
         files: 'scss/**/*.scss',
-        tasks: ['sass']
+        tasks: ['sass:watch']
       },
+      uglify: {
+        files: ['bower_components/foundation/js/*', 'js/*'],
+        tasks: 'uglify:watch'
+      }
     }
   });
 
@@ -46,6 +72,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass','uglify']);
-  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('build', ['sass:build','uglify:build']);
+  grunt.registerTask('default', ['watch']);
 }
